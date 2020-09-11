d6418b5795c5d6d8582eada609da44e4bfbdfca0
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-11 15:03:56 -0300

    Add base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Wrapper, Container } from './App.styles'
+import AppHeader from '../AppHeader'
+import AppContainer from '../AppContainer'
 
 function App () {
     return <Wrapper>
         <Container>
-            TODO: app
+            <AppHeader />
+            <AppContainer 
+                left={<div>
+                    Produtos disponíveis 
+                </div>}
+
+                middle={<div>
+                    Sua lista de compras
+                </div>}
+
+                right={<div>
+                    Estatísticas    
+                </div>}
+            />
         </Container>
     </Wrapper>
 }
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Wrapper } from './AppHeader.styles'
+
+function AppHeader() {
+    return <Wrapper>
+        Alga
+        <span>
+            Shopping
+        </span>
+    </Wrapper>
+}
+
+export default AppHeader;
